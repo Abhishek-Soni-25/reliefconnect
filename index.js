8091e442c33945ba900e981829780c58fb697afd
Abhishek-Soni-25 <abhi250604@gmail.com>
2025-04-13 17:12:56 +0530

    resolved port issue

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // index.js 
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
